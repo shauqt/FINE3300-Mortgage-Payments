c4744325685d6ecf7962efaa57f9e2cfd03d0f47
shauqt <193943014+shauqt@users.noreply.github.com>
2025-01-27 23:46:55 -0500

    starting payment calculation function

---
+++ calculations.py
@@ -0,0 +1,9 @@
+# Create a function that will calculate the monthly, semi-monthly, bi-weekly, weekly, rapid bi-weekly, and rapid weekly payments
+
+def mortgage_payments(principal, interest, ammortization):
+
+    
+
+
+
+    return
