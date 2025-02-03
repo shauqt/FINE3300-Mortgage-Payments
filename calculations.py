e6f6d610160d1a9d5522fa25f59fa5c90febc129
shauqt <193943014+shauqt@users.noreply.github.com>
2025-02-03 01:03:14 -0500

    updating grammatical errors

--- calculations.py
+++ calculations.py
@@ -1,5 +1,5 @@
 # Create a function that will calculate the monthly, semi-monthly, bi-weekly, rapid bi-weekly, weekly, and rapid weekly payments
-def mortgage_payments(principal, rate, ammortization):
+def mortgage_payments(principal, rate, amortization):
 
     # Calculate the rates for each period 
     # Note: rapid bi_weekly and rapid weekly do not have rates as their payment calculations are based of monthly payments
@@ -9,11 +9,11 @@ def mortgage_payments(principal, rate, ammortization):
     weekly_rate = ((1 + (rate/2)) ** (2/52)) - 1
 
     # Using the rates, calculate the payments 
-    monthly_payment = ((monthly_rate * (1 + monthly_rate) ** (ammortization * 12)) / ((1 + monthly_rate) ** (ammortization * 12) - 1)) * principal
-    semi_monthly_payment = ((semi_monthly_rate * (1 + semi_monthly_rate) ** (ammortization * 24)) / ((1 + semi_monthly_rate) ** (ammortization * 24) - 1)) * principal
-    bi_weekly_payment = ((bi_weekly_rate * (1 + bi_weekly_rate) ** (ammortization * 26)) / ((1 + bi_weekly_rate) ** (ammortization * 26) - 1)) * principal
+    monthly_payment = ((monthly_rate * (1 + monthly_rate) ** (amortization * 12)) / ((1 + monthly_rate) ** (amortization * 12) - 1)) * principal
+    semi_monthly_payment = ((semi_monthly_rate * (1 + semi_monthly_rate) ** (amortization * 24)) / ((1 + semi_monthly_rate) ** (amortization * 24) - 1)) * principal
+    bi_weekly_payment = ((bi_weekly_rate * (1 + bi_weekly_rate) ** (amortization * 26)) / ((1 + bi_weekly_rate) ** (amortization * 26) - 1)) * principal
     rapid_bi_weekly_payment = monthly_payment / 2 
-    weekly_payment = ((weekly_rate * (1 + weekly_rate) ** (ammortization * 52)) / ((1 + weekly_rate) ** (ammortization * 52) - 1)) * principal
+    weekly_payment = ((weekly_rate * (1 + weekly_rate) ** (amortization * 52)) / ((1 + weekly_rate) ** (amortization * 52) - 1)) * principal
     rapid_weekly_payment = monthly_payment / 4
     
     # Return payment values, rounding to two decimal places
@@ -32,10 +32,10 @@ def main():
     # Store user inputs for principal, interest, and ammortization as variables
     principal_input = float(input("Please provide the principal amount: "))
     interest_input = float(input("Please provide the interest rate: ")) / 100
-    ammortization_input = int(input("Please provide ammorization period: "))
+    amortization_input = int(input("Please provide the ammorization period in years: "))
 
     # Call the mortgage payments function and store the values into the variables
-    monthly_payment, semi_monthly_payment, bi_weekly_payment, rapid_bi_weekly_payment, weekly_payment, rapid_weekly_payment = mortgage_payments(principal_input, interest_input, ammortization_input)
+    monthly_payment, semi_monthly_payment, bi_weekly_payment, rapid_bi_weekly_payment, weekly_payment, rapid_weekly_payment = mortgage_payments(principal_input, interest_input, amortization_input)
 
     # Print out the calculated values
     print(f"Your Monthly Payment Is: ${monthly_payment}")
